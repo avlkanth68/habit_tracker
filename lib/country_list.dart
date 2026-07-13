8b873872afa9bcbf71209798cd41b7e1d9ec195c
AMBLE VENKATARAMU LAKSHMIKANTH <2109203106@onlinesmu.edu.in>
2026-07-13 03:25:54 -0400

    updated the countylist.dart

--- lib/country_list.dart
+++ lib/country_list.dart
@@ -8,3 +8,23 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'country_list.dart';
 import 'habit_tracker_screen.dart';
 import 'login_screen.dart';
+
+// country_list.dart
+
+Future<List<String>> fetchCountries() async {
+  List<String> countries = [
+    'United States',
+    'Canada',
+    'United Kingdom',
+    'Australia',
+    'India',
+    'Germany',
+    'France',
+    'Japan',
+    'China',
+    'Brazil',
+    'South Africa'
+  ];
+
+  return countries;
+}
