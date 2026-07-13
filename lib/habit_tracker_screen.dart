d7e5cef0a6df4221fcb973f2d477d12984a761f5
AMBLE VENKATARAMU LAKSHMIKANTH <2109203106@onlinesmu.edu.in>
2026-07-12 23:12:38 -0400

    fivedart files

--- lib/habit_tracker_screen.dart
+++ lib/habit_tracker_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'add_habit_screen.dart';
 
 class HabitTrackerScreen extends StatefulWidget {
   final String username;
@@ -180,7 +181,14 @@ class _HabitTrackerScreenState extends State<HabitTrackerScreen> {
       ),
       floatingActionButton: selectedHabitsMap.isEmpty
           ? FloatingActionButton(
-              onPressed: () {},
+              onPressed: () 
+              { Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => AddHabitScreen(),
+                    ),
+                );
+            },
               backgroundColor: Colors.blue.shade700,
               tooltip: 'Add Habits',
               child: const Icon(Icons.add),
